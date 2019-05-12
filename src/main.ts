895f01e5cf1a70f07dae61b1248f73165ef99d9f
BarryPietersen <barrypietersen@hotmail.com>
2019-05-13 00:02:21 +1000

    preload images after app initializes

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { Suits, Ranks } from './app/types/card';
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +11,23 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+window.onload = function () {
+  preloadImages();
+}
+
+function preloadImages() {
+  let rank: string;
+  let img: HTMLImageElement;
+  let path = './assets/images/JPEG/';
+  let imgStorage = document.getElementById('imageStore');
+
+  for(let s = 0; s < 4; s++) {
+    for(let r = 0; r < 13; r++) {
+      rank = r === 0 || r > 9 ? Ranks[r][0] : (r + 1).toString();
+      img = new Image();
+      img.src = `${path}${rank}${Suits[s][0]}.jpg`; 
+      imgStorage.append(img);
+    }
+  }
+}
