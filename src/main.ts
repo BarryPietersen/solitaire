da79cc96a1598cc517444d4587ba4d248ee3b69e
BarryPietersen <barrypietersen@hotmail.com>
2019-05-13 20:04:15 +1000

    preload images after app initializes

--- src/main.ts
+++ src/main.ts
@@ -20,14 +20,18 @@ function preloadImages() {
   let rank: string;
   let img: HTMLImageElement;
   let path = './assets/images/JPEG/';
-  let imgStorage = document.getElementById('imageStore');
+  let preloaded = document.createElement('div');
+  preloaded.id = 'preloaded';
+  preloaded.hidden = true;
 
   for(let s = 0; s < 4; s++) {
     for(let r = 0; r < 13; r++) {
       rank = r === 0 || r > 9 ? Ranks[r][0] : (r + 1).toString();
       img = new Image();
-      img.src = `${path}${rank}${Suits[s][0]}.jpg`; 
-      imgStorage.append(img);
+      img.src = `${path}${rank}${Suits[s][0]}.jpg`;
+      preloaded.append(img);
     }
   }
+
+  document.getElementsByTagName('body')[0].append(preloaded);
 }
